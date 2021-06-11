60d108687b393bc68fada5fca422f3afa26f6ca3
Cristian Restrepo <crestrepo21@gmail.com>
2021-06-11 10:35:14 -0700

    grabbing and dropping items from room. AB#41

--- src/com/locallampoon/fiveh/core/GameState.java
+++ src/com/locallampoon/fiveh/core/GameState.java
@@ -5,51 +5,53 @@ import java.io.IOException;
 
 class GameState {
 
-     private static String quitSave(BufferedReader saveReader) throws IOException {
-         String saveResponse;
+    private static String quitSave(BufferedReader saveReader) throws IOException {
+        String msg;
+        String saveCommand;
 
-         do {
-             System.out.println("Do you want to save the game?");
-             saveResponse = saveReader.readLine().toLowerCase();
+        while (true) {
+            System.out.println("Do you want to save the game?");
+            String saveResponse = saveReader.readLine().toLowerCase();
 
-             switch (saveResponse) {
-                 case "no":
-                 case "n":
-                     return "q";
-                 case "yes":
-                 case "y":
-                     System.out.println("Please name your save file:  >   ");
-                     String saveFileName = saveReader.readLine();
-                     //TODO: Write a save game/load game methods
-                     //                     saveGame(saveFileName);
-                     return "q";
-                 case "continue":
-                 case "c":
-                     return "i";
-             }
-             System.out.println("INVALID RESPONSE: Enter Y to save and quit \n\t N to quit without saving \n\t C to continue playing");
-
-         } while (true);
+            if ("yes".equals(saveResponse) || "y".equals(saveResponse)) {
+                System.out.println("Please name your save file:  >   ");
+                String saveFileName = saveReader.readLine();
+                //TODO: Write a save game/load game methods
+                //                     saveGame(saveFileName);
+                saveCommand = "q";
+                break;
+            } else if ("no".equals(saveResponse) || "n".equals(saveResponse)) {
+                saveCommand = "q";
+                break;
+            } else if ("continue".equals(saveResponse) || "c".equals(saveResponse)) {
+                saveCommand = "i";
+                break;
+            } else {
+                msg = "INVALID RESPONSE: \n\t Enter Y to save and quit \n\t N to quit without saving \n\t C to continue playing";
+                System.out.println(msg);
+            }
+        }
+        return saveCommand;
     }
 
     public static String quitHelper(BufferedReader quitReader) throws IOException {
         String quitCommand;
-        do {
-            System.out.println("Are you sure you want to quit the game?");
+        System.out.println("Are you sure you want to quit the game?");
+
+        while (true) {
             String quitResponse = quitReader.readLine().toLowerCase();
 
+            System.out.println(quitResponse);
             if (quitResponse.equals("no") || quitResponse.equals("n")) {
                 quitCommand = "i";
                 break;
-
-            }
-            else if (quitResponse.equals("yes") || quitResponse.equals("y")){
+            } else if (quitResponse.equals("yes") || quitResponse.equals("y")) {
                 quitCommand = quitSave(quitReader);
                 break;
+            } else {
+                System.out.println("INVALID RESPONSE: \n\tEnter Y to quit \n\t N to continue");
             }
-            System.out.println("INVALID RESPONSE: Enter Y to quit \n\t N to continue");
-        } while (true);
-
+        }
         return quitCommand;
     }
 }
