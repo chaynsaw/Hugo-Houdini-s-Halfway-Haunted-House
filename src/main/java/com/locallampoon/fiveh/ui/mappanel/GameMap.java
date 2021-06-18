acd2395780b77d922eb3eba0a4ae26bd5664ecce
wang.xander <wang.xander@gmail.com>
2021-06-18 08:18:06 -0700

    AB#488 Updated GameMap that handles MapRoom parent Room

--- src/main/java/com/locallampoon/fiveh/ui/mappanel/GameMap.java
+++ src/main/java/com/locallampoon/fiveh/ui/mappanel/GameMap.java
@@ -1,17 +1,18 @@
 package com.locallampoon.fiveh.ui.mappanel;
 
+import com.locallampoon.fiveh.core.Room;
+
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * load rooms and control positions on the map
  */
-class GameMap {
+public class GameMap {
     private static GameMap gameMapInstance = new GameMap();
-    private List<Room> rooms = new ArrayList<>();
+    private List<Room> mapRooms = new ArrayList<>();
 
     private GameMap(){
-        this.demoMap();
     }
 
     public static GameMap getInstance(){
@@ -21,14 +22,7 @@ class GameMap {
         return GameMap.gameMapInstance;
     }
 
-    private void demoMap(){
-        this.rooms.add(new Room("room1", GameMapPanel.UNIT_SIZE*10, GameMapPanel.UNIT_SIZE*10));
-        this.rooms.add(new Room("room2", GameMapPanel.UNIT_SIZE*28, GameMapPanel.UNIT_SIZE*32));
-        this.rooms.add(new Room("room3", GameMapPanel.UNIT_SIZE*18, GameMapPanel.UNIT_SIZE*40));
-        this.rooms.add(new Room("room4", GameMapPanel.UNIT_SIZE*40, GameMapPanel.UNIT_SIZE*18));
-    }
-
     public List<Room> getRooms(){
-        return this.rooms;
+        return this.mapRooms;
     }
 }
