15b99418239bb328d1401e2fb3453ccf0d00eeea
wang.xander <wang.xander@gmail.com>
2021-06-18 07:02:40 -0700

    AB#482 Added mouse listener for next iteration

---
+++ src/main/java/com/locallampoon/fiveh/ui/mappanel/GameMapMouseListener.java
@@ -0,0 +1,40 @@
+package com.locallampoon.fiveh.ui.mappanel;
+import javax.swing.event.MouseInputListener;
+import java.awt.event.MouseEvent;
+
+class GameMapMouseListener implements MouseInputListener {
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        System.out.printf("clicked at location:%s\n", e.getLocationOnScreen());
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
+}
