3b58ba4648a7f291e7e476cbf4db8a2d2253bbcf
wang.xander <wang.xander@gmail.com>
2021-06-18 06:56:42 -0700

    AB#446 Changed background color to black

--- src/main/java/com/locallampoon/fiveh/ui/MapPanel.java
+++ src/main/java/com/locallampoon/fiveh/ui/MapPanel.java
@@ -1,5 +1,7 @@
 package com.locallampoon.fiveh.ui;
 
+import com.locallampoon.fiveh.ui.mappanel.GameMapPanel;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -7,10 +9,10 @@ public class MapPanel {
     JPanel panel;
 
     public MapPanel() {
-        panel = new JPanel();
+        panel = new GameMapPanel();
         panel.setEnabled(false);
         panel.setBounds(0, 0, 600, 500);
-        panel.setBackground(Color.GREEN);
+        panel.setBackground(Color.black);
     }
 
     public JPanel getPanel() {
