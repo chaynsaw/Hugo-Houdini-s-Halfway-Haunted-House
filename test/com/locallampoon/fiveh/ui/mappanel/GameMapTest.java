b797851110967d55e9cb794d27657af4d3ac4a60
wang.xander <wang.xander@gmail.com>
2021-06-21 09:48:16 -0700

    Removed out folder

---
+++ test/com/locallampoon/fiveh/ui/mappanel/GameMapTest.java
@@ -0,0 +1,12 @@
+package com.locallampoon.fiveh.ui.mappanel;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameMapTest {
+
+    @Test
+    public void getRooms() {
+    }
+}
