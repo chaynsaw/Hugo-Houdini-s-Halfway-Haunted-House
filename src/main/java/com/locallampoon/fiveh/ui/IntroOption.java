c54f48b79b30dc6b47acca97cb1ee8c60df13548
raymond-sde <raymond.aranda.sde@gmail.com>
2021-06-21 15:28:01 -0700

    AB#553, simplified enum class

--- src/main/java/com/locallampoon/fiveh/ui/IntroOption.java
+++ src/main/java/com/locallampoon/fiveh/ui/IntroOption.java
@@ -1,21 +1,6 @@
 package com.locallampoon.fiveh.ui;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public enum IntroOption {
+    // TODO: add help
     NEW(), QUIT();
-
-    private static final List<IntroOption> OPTIONS;
-
-    static {
-        OPTIONS = new ArrayList<>();
-        // add all options to List
-        Collections.addAll(OPTIONS, IntroOption.values());
-    }
-
-    public static List<IntroOption> getOptions() {
-        return Collections.unmodifiableList(OPTIONS);
-    }
 }
