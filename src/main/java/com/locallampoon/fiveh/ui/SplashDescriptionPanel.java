5996701597e7251dde380332b64ca4460b58e7fa
Chaynor Hsiao <chaynsaw@gmail.com>
2021-06-26 08:18:21 -0700

    Text edits only

--- src/main/java/com/locallampoon/fiveh/ui/SplashDescriptionPanel.java
+++ src/main/java/com/locallampoon/fiveh/ui/SplashDescriptionPanel.java
@@ -7,8 +7,11 @@ import java.awt.*;
 
 public class SplashDescriptionPanel {
     private static final String winText = """
-                    \tA passageway is opened to you and it glows a warm blue, like the keys in your pack, brighter and brighter as you approach. Its warmth lifts the spirits and fills your wearied mind with hope... but each time you pass through it - nothing. You transport to the same place. It matters not how many times you cross.
-                    \tThe realization that dawns upon you is as clear as it is maddening and final. There is no end. There is no beginning. There is no escape. This is a prison.
+                    A passageway is opened to you and it glows a warm blue, like the keys in your pack, brighter and brighter as you approach. Its warmth lifts the spirits and fills your wearied mind with hope... but each time you pass through it - nothing happens. You transport back to the other side of the room. It matters not how many times you cross.
+                    
+                    The realization that dawns upon you is as clear as it is maddening and final. 
+                    
+                    There is no end. There is no beginning. There is no escape. This is a prison.
                     """;
     JPanel panel;
     JTextArea description;
